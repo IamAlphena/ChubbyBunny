fe848f7d97c6621b7729187a7f8f5b8c907ac641
Alphena <alphena@gmail.com>
2021-04-08 23:31:45 -0700

    updated routes and chat css

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,5 @@
+//get userID and verifying it exists
+
+//post: create new user
+
+//update profile :/id {image/email}
--- models/chat.js
+++ models/chat.js
@@ -2,29 +2,36 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 
-class Chat extends Model {}
+class Chat extends Model { }
 
 Chat.init(
-    {
-        id:{
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          primaryKey: true,
-          autoIncrement: true,
-        },
-        name:{
-          type: DataTypes.STRING,
-          allowNull: false,  
-        },
-        post:{
-          type: DataTypes.STRING,
-          allowNull: false, 
-         },
-         user_id: {
-           type: DataTypes.INTEGER,
-           foreignKey: true,
-         }
-    }
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      foreignKey: true,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'chat',
+  }
 )
 
 
