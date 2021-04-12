09dc43c32c4f0ac72b87d8c1e3b0c344c34a2876
jcerb <brt.jst@gmail.com>
2021-04-11 20:26:32 -0700

    register js

---
+++ public/js/logout.js
@@ -0,0 +1,16 @@
+const logout = async () => {
+    alert("heard this");
+    // alert is for testing, will need to be removed.
+    const response = await fetch('/api/users/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+      document.location.replace('/login');
+    } else {
+      alert(response.statusText);
+    }
+  };
+  
+  document.querySelector('#logout').addEventListener('click', logout);
